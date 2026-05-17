5c88aab3451dd4d0ad497c67e17e078ac762c572
sakthimanikandan558 <sakthimanikandan42@gmail.com>
2026-05-17 13:37:24 +0530

    feat: implement user authentication flow with Inertia, Vue, and Laravel controllers

--- inertia-todo-app/app/Http/Middleware/HandleInertiaRequests.php
+++ inertia-todo-app/app/Http/Middleware/HandleInertiaRequests.php
@@ -35,9 +35,16 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return [
-            ...parent::share($request),
-            //
-        ];
+        return array_merge(parent::share($request), [
+            'auth' => [
+                'user' => $request->user(),
+            ],
+
+            'errors' => function () use ($request) {
+                return $request->session()->get('errors')
+                    ? $request->session()->get('errors')->getBag('default')->getMessages()
+                    : (object) [];
+            },
+        ]);
     }
 }
